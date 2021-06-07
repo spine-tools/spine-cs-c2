83166232c90d33af20d37a71c5b36cff2d6c190e
Kristof Phillips <Kristof.phillips@kuleuven.be>
2021-06-07 16:15:59 +0200

    First version of restructured project

--- .spinetoolbox/specifications/Tool/spineopt.jl
+++ .spinetoolbox/specifications/Tool/spineopt.jl
@@ -4,7 +4,10 @@ using Gurobi
 using JuMP
 using Cbc
 optimizer = optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => 0, "MIPGap" => 1e-2)
-m = run_spineopt(ARGS...; mip_solver=optimizer, lp_solver=optimizer)
+db_url_in = "sqlite:///C:\\Users\\u0138303\\Documents\\Spine_repos\\spine-cs-c2\\.spinetoolbox\\items\\input\\input.sqlite"
+db_url_out = "sqlite:///C:\\Users\\u0138303\\Documents\\Spine_repos\\spine-cs-c2\\.spinetoolbox\\items\\output\\output.sqlite"
+
+m = run_spineopt(db_url_in,db_url_out, mip_solver=optimizer, lp_solver=optimizer)
 # m = run_spineopt(ARGS...)
 
 # optimizer = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
