492f055d3512a4efc46a79ad3b71f25230119f5b
Juha Kiviluoma <juha.kiviluoma@vtt.fi>
2021-12-03 10:24:39 +0200

    Ignoring import errors (should not affect runs). Changed to CBC as default solver. There is still a problem running recent versions of SpineOpt.

--- .spinetoolbox/specifications/Tool/spineopt.jl
+++ .spinetoolbox/specifications/Tool/spineopt.jl
@@ -1,11 +1,12 @@
 using Revise
 using SpineOpt
-using Gurobi
+#using Gurobi
 using JuMP
 using Cbc
 
-optimizer = optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => 0, "MIPGap" => 1e-2)
+optimizer = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
 m = run_spineopt(ARGS...; mip_solver=optimizer, lp_solver=optimizer)
 
 #optimizer = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
+#optimizer = optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => 0, "MIPGap" => 1e-2)
 #m = run_spineopt(ARGS...)
