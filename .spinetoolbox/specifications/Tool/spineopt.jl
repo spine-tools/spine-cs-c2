a255712beef27afdf6fb6e22f6fc8647b5acc9b6
Manuel <chacarero@gmail.com>
2021-12-03 08:40:19 +0100

    Replace n490 tool by DC with the excel data

--- .spinetoolbox/specifications/Tool/spineopt.jl
+++ .spinetoolbox/specifications/Tool/spineopt.jl
@@ -1,11 +1,11 @@
 using Revise
 using SpineOpt
-# sing Gurobi
+using Gurobi
 using JuMP
 using Cbc
 
-# optimizer = optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => 0, "MIPGap" => 1e-2)
-# m = run_spineopt(ARGS...; mip_solver=optimizer, lp_solver=optimizer)
+optimizer = optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => 0, "MIPGap" => 1e-2)
+m = run_spineopt(ARGS...; mip_solver=optimizer, lp_solver=optimizer)
 
-optimizer = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
-m = run_spineopt(ARGS...)
+#optimizer = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
+#m = run_spineopt(ARGS...)
